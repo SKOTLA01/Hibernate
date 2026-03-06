c74ca4d85fd634f1c84121a75aa4e07c6e93211a
SKOTLA01 <saiteja.kotla1@gmail.com>
2026-03-06 14:56:41 +0530

    Added XML configuration,Entity

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,36 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Student s1=new Student();
+        s1.setsAge(24);
+        s1.setsName("Saiteja");
+        s1.setRollNo(107);
+
+        Configuration configuration=new Configuration();
+configuration.addAnnotatedClass(Student.class);
+
+        configuration.configure();
+
+        Session session;
+        try (SessionFactory sf = configuration.buildSessionFactory()) {
+
+            session = sf.openSession();
+        }
+        Transaction transaction=session.beginTransaction();
+
+        session.persist(s1);
+    transaction.commit();
+        System.out.println(s1);
+
 
     }
 }
